f833dcdafc2a6851063968665d656f545e7bfe8b
Rake11 <144303791+Rake11@users.noreply.github.com>
2024-11-26 21:40:57 -0800

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,19 @@
+document.getElementById("box1").innerHTML=`
+<img src='https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649' width=400px/>
+<h2>Nature</h2>`
+
+document.getElementById("box2").innerHTML=`
+<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEs1F2-qh4tQqYVM0RmY8xLECwLY3WNGQcnkGw15yFXL1DCXSNROuffY26fsbL1_qGO0c&usqp=CAU' width=400px/>
+<h2>Nature</h2>`
+
+let card1=document.createElement("div")
+card1.innerHTML=`<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRflI2Ho9ajdgBdxqrQwymoBanOJ4VN1IqIdOd_BH6x9XJBUtwgNPe9f6s-ywQxGs3w0Y&usqp=CAU' width=400px/>
+<h2>Nature</h2>`
+card1.className="card1"
+document.body.appendChild(card1)
+
+let card2=document.createElement("div")
+card2.innerHTML=`<img src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg' width=400px/>
+<h2>Nature</h2>`
+card2.className="card1"
+document.body.appendChild(card2)
