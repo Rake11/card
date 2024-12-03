d1bcc1d4e7c027be84ac7fb3e9e264b743b63a1b
Rake11 <144303791+Rake11@users.noreply.github.com>
2024-12-03 11:16:04 +0530

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,19 +1,19 @@
 document.getElementById("box1").innerHTML=`
-<img src='https://fps.cdnpk.net/images/home/subhome-ai.webp?w=649&h=649' width=400px/>
+<img src='https://m.economictimes.com/thumb/msid-61166379,width-1200,height-900,resizemode-4,imgsize-126503/why-after-13-years-mahendra-singh-dhoni-is-still-not-out.jpg' width=400px/>
 <h2>Nature</h2>`
 
 document.getElementById("box2").innerHTML=`
-<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEs1F2-qh4tQqYVM0RmY8xLECwLY3WNGQcnkGw15yFXL1DCXSNROuffY26fsbL1_qGO0c&usqp=CAU' width=400px/>
+<img src='https://cdn.britannica.com/48/252748-050-C514EFDB/Virat-Kohli-India-celebrates-50th-century-Cricket-November-15-2023.jpg' width=400px/>
 <h2>Nature</h2>`
 
 let card1=document.createElement("div")
-card1.innerHTML=`<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRflI2Ho9ajdgBdxqrQwymoBanOJ4VN1IqIdOd_BH6x9XJBUtwgNPe9f6s-ywQxGs3w0Y&usqp=CAU' width=400px/>
+card1.innerHTML=`<img src='https://static.toiimg.com/thumb/msid-111741230,width-1070,height-580,imgsize-37996,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg' width=400px/>
 <h2>Nature</h2>`
 card1.className="card1"
 document.body.appendChild(card1)
 
 let card2=document.createElement("div")
-card2.innerHTML=`<img src='https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg' width=400px/>
+card2.innerHTML=`<img src='https://wallpapers.com/images/featured/jadeja-pictures-9idqtlrdq16fbokn.jpg' width=400px/>
 <h2>Nature</h2>`
 card2.className="card1"
 document.body.appendChild(card2)
